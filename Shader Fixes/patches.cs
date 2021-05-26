caea7a7e75b852be7a2d3d7156c475ab85fe0213
TheRealBeef <michael.d.friske@outlook.com>
2021-05-26 11:43:18 +0200

    Add bloom, volumetric changes

--- Shader Fixes/patches.cs
+++ Shader Fixes/patches.cs
@@ -5,9 +5,13 @@ using Assets.Scripts;
 
 namespace Shader_Fixes
 {
-	
-	// Fixes to SSAO
-	[HarmonyPatch(typeof(CameraController), "SetAmbientOcclusion", new Type[] { })]
+    public class PatchConfigs
+    {
+        public static bool I_Have_A_Strong_PC_And_GPU = false; // Determines whether to use quarter or half resolution, and how many samples.
+    }
+
+    // Fixes to SSAO
+    [HarmonyPatch(typeof(CameraController), "SetAmbientOcclusion", new Type[] { })]
     public class SSAOPatcher
     {
         public static bool tex_generated = false;
@@ -26,15 +30,15 @@ namespace Shader_Fixes
             float f = 0.0f;
             float result = 0.0f;
 
-            for (int x = 0;x < size; x++)
+            for (int x = 0; x < size; x++)
             {
                 u = Mathf.Floor(x / pix_size);
-                for (int y = 0; y <size; y++)
+                for (int y = 0; y < size; y++)
                 {
                     v = Mathf.Floor(y / pix_size);
                     f = 0.06711056f * u + 0.00583715f * v;
                     result = fractional(52.9829189f * fractional(f));
-                    noise_tex.SetPixel(x,y,new Color (result, result, result, result)); // Only W channel matters here
+                    noise_tex.SetPixel(x, y, new Color(result, result, result, result)); // Only W channel matters here
                     // BeefShaders.BeefShaders.AppendLog("Generating, curently on row " + y.ToString() +" and column " + x.ToString());
                 }
             }
@@ -60,7 +64,7 @@ namespace Shader_Fixes
             SSAOPatch.Downsampling = 2;
             SSAOPatch.Intensity = 8.0f;
             SSAOPatch.LumContribution = 0.20f;
-            
+
             //// GUH ////
             if (!tex_generated)
             {
@@ -71,4 +75,79 @@ namespace Shader_Fixes
             }
         }
     }
+
+    // Changes to Bloom
+    [HarmonyPatch(typeof(UltimateBloom),"Awake")]
+    public class BloomPatcher
+    {
+        static void Postfix(UltimateBloom __instance)
+        {
+            UltimateBloom TonemapPatcher = __instance as UltimateBloom;
+
+            TonemapPatcher.m_BloomIntensity = 0.15f;
+            TonemapPatcher.m_BloomThreshhold = 1.1f;
+            TonemapPatcher.m_HDR = UltimateBloom.HDRBloomMode.On;
+            TonemapPatcher.m_IntensityManagement = UltimateBloom.BloomIntensityManagement.Threshold;
+        }
+    }
+
+    // Changes to Volumetric Lighting
+    [HarmonyPatch(typeof(VolumetricLight), "VolumetricLightRenderer_PreRenderEvent")]
+    public class VolumetricPatcher
+    {
+        static void Prefix(VolumetricLight __instance)
+        {
+            VolumetricLight VolumetricPatcher = __instance as VolumetricLight;
+            if (PatchConfigs.I_Have_A_Strong_PC_And_GPU == true)
+            {
+                VolumetricPatcher.SampleCount = 4; // 1;
+                VolumetricPatcher.MaxRayLength = 200.0f; // 200.0f;
+            }
+            else
+            {
+                VolumetricPatcher.SampleCount = 2; // 1;
+                VolumetricPatcher.MaxRayLength = 100.0f; // 200.0f;
+            }
+
+            
+            VolumetricPatcher.ExtinctionCoef = 0.01f;    // 0.0211f;
+            VolumetricPatcher.MieG = 0.25f;              // 0.321f;
+            VolumetricPatcher.ScatteringCoef = 0.2f;    // 0.176f;
+            VolumetricPatcher.SkyboxExtinctionCoef = 0.01f; // 0.246f;
+        }
+    }
+
+    // Changes to Volumetric Lighting Part II
+    [HarmonyPatch(typeof(VolumetricLightRenderer), "Awake")]
+    public class VolumetricPatcherPt2
+    {
+        static void Postfix(VolumetricLightRenderer __instance)
+        {
+            VolumetricLightRenderer VolumetricPatcherPt2 = __instance as VolumetricLightRenderer;
+            if (PatchConfigs.I_Have_A_Strong_PC_And_GPU == true)
+            {
+                VolumetricPatcherPt2.Resolution = VolumetricLightRenderer.VolumtericResolution.Half;
+            }
+            else
+            {
+                VolumetricPatcherPt2.Resolution = VolumetricLightRenderer.VolumtericResolution.Quarter;
+            }
+        }
+    }
+    /*
+        // Fixes to Tonemapping
+        [HarmonyPatch(typeof(DeluxeFilmicCurve), "Awake")]
+        public class BloomPatcher
+        {
+            static void Postfix(UltimateBloom __instance)
+            {
+                UltimateBloom TonemapPatcher = __instance as UltimateBloom;
+
+                // Bloom Settings
+                TonemapPatcher.m_BloomIntensity = 0.15f;
+                TonemapPatcher.m_BloomThreshhold = 1.1f;
+                TonemapPatcher.m_HDR = UltimateBloom.HDRBloomMode.On;
+                TonemapPatcher.m_IntensityManagement = UltimateBloom.BloomIntensityManagement.Threshold;
+            }
+        } */
 }
